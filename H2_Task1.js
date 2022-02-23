5e1efcdbd72add41d893c8dddfd0d243ee87cc5c
OleksandrVeremchuk <99984550+OleksandrVeremchuk@users.noreply.github.com>
2022-02-23 09:24:17 +0200

    Add files via upload

--- H2_Task1.js
+++ H2_Task1.js
@@ -1,3 +1,25 @@
-let input1 = +prompt('Please, input 5'),
-    input2 = +prompt('Please, input 8');
-alert(input1 + input2)
+//H2_task_1
+
+//let input1 = +prompt('Please, input 5'),
+//    input2 = +prompt('Please, input 8');
+//alert(input1 + input2)
+
+
+//H2_task_2
+
+//let input = prompt('Please, input your name')
+//alert('Hello, ' + input)
+
+//H2_task_3
+
+//Example 1
+
+//let a = 5, b = 2
+//alert (a % b)
+
+//Example 2
+//let a = 80, b = 2
+//alert (a % b)
+
+//H2_task_4
+
